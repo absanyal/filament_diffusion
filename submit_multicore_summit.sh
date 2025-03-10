33f456155b59bd7f81a1ea3512845b1c4fc77200
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-03-10 11:07:14 -0400

    feat: Add submission scripts for multicore jobs on PC and Summit environments

---
+++ submit_multicore_summit.sh
@@ -0,0 +1,22 @@
+#BSUB -P BIP240
+#BSUB -J filament
+#BSUB -W 2:00 
+#BSUB -nnodes 1
+#BSUB -alloc_flags "maximizegpfs smt1 gpumps gpudefault"
+### End BSUB Options and begin shell commands
+
+module load gcc
+
+mkdir -p outrun
+mkdir -p data
+mkdir -p dump
+
+make clean
+make
+
+nres=1
+
+for sim_i in {1..${nres}}
+do
+    jsrun -n 1 -a1 -c1 -g0 -r1  ./filament input_files/input.${sim_i}.in > outrun/out.run.${sim_i} &
+done
