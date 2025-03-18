196cfcdedfcd312125cac6e33a48f7ebd4375b16
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-03-18 15:42:57 -0400

    feat: Add curve fitting for survival function analysis in attachtime_vs_bounces.py

--- batch_analysis/attachtime_vs_bounces.py
+++ batch_analysis/attachtime_vs_bounces.py
@@ -3,12 +3,21 @@ import matplotlib.pyplot as plt
 import os
 import sys
 import inspect
+from scipy.optimize import curve_fit
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 import modules.rcparams
+
+#-----------------------------------------------------------------------------------------------
+
+k_react = 0.001
+dt = 1.0
+
+def rate(k, t):
+    return np.exp(-k*t)
 
 folder_count = 0  # type: int
 input_path = "../runs"  # type: str
@@ -33,31 +42,6 @@ for sim_i in range(sim_count):
 
 print("Number of successful attach times: {}".format(successful_attach_times))
 
-# fig, ax = plt.subplots(1, 3, figsize=(12, 4))
-
-# ax[0].scatter(attach_times, bounces, s=10, c='k', alpha=0.5)
-# ax[0].set_xlabel("Attachment time (ms)")
-# ax[0].set_ylabel("Number of bounces")
-
-# ax[1].hist(attach_times, bins='auto', color='k', alpha=0.5)
-# ax[1].set_xlabel("Attachment time (ms)")
-# ax[1].set_ylabel("Frequency")
-
-# avg_attach_time = np.mean(attach_times)
-
-# ax[1].axvline(avg_attach_time, color='r', linestyle='--', label="Average attach time: {:.2f} ms".format(avg_attach_time))
-# ax[1].legend()
-
-# ax[2].hist(bounces, bins='auto', color='k', alpha=0.5)
-# ax[2].set_xlabel("Number of bounces")
-# ax[2].set_ylabel("Frequency")
-
-# avg_bounces = np.mean(bounces)
-
-# ax[2].axvline(avg_bounces, color='r', linestyle='--', label="Average bounces: {:.2f}".format(avg_bounces))
-# ax[2].legend()
-
-# plt.tight_layout()
 
 plt.figure(figsize=(5, 5))
 
@@ -69,14 +53,55 @@ plt.xscale('log')
 plt.xlabel("Attachment time (ms)")
 plt.ylabel("Number of bounces")
 
-plt.savefig("attachtime_vs_bounces.pdf", format='pdf', bbox_inches='tight')
+plt.savefig("attachtime_vs_bounces.{}.pdf".format(k_react), format='pdf', bbox_inches='tight')
+
+plt.title(r'$k_{{\mathrm{{react}}}} = {:.3f}$, $\mathrm{{d}}t = {:.2f}$'.format(k_react, dt))
 
 plt.clf()
 plt.cla()
 
 plt.figure(figsize=(5, 5))
 
-plt.hist(attach_times, bins='auto', color='b', alpha=0.5)
+counts, bins = np.histogram(attach_times, bins='auto', density=True)
+dx = bins[1] - bins[0]
+
+pdf_area = np.trapz(counts, bins[:-1], dx=dx)
+
+counts = counts / pdf_area
+pdf_area = np.trapz(counts, bins[:-1], dx=dx)
+print("PDF area: {}".format(pdf_area))
+
+CDF = np.zeros_like(counts)
+for i in range(len(counts)):
+    CDF[i] = np.trapz(counts[:i+1], bins[:i+1], dx=dx)
+
+survival = 1 - CDF
+
+p_opt, p_cov = curve_fit(rate, bins[:-1], survival, p0=[0.01])
+
+k = p_opt[0]
+print("k: {}".format(k))
+print("1/k: {}".format(1/k))
+print("<t> : {}".format(np.mean(attach_times)))
+
+y_fit = rate(bins[:-1], k)
+
+plt.plot(bins[:-1], survival, 'o', label="Survival function")
+plt.plot(bins[:-1], y_fit, 'r-', label=r"$k_{{\mathrm{{attach}}}} = {:.2f}$".format(k))
+plt.yscale('log')
+
+plt.title(r'$k_{{\mathrm{{react}}}} = {:.3f}$, $\mathrm{{d}}t = {:.2f}$'.format(k_react, dt))
+
+plt.legend()
+
+plt.savefig("kreact.{}.pdf".format(k_react), format='pdf', bbox_inches='tight')
+
+plt.clf()
+plt.cla()
+
+plt.figure(figsize=(5, 5))
+
+plt.hist(attach_times, bins='auto', color='b', alpha=0.5, density=True)
 
 plt.xlabel("Attachment time (ms)")
 plt.ylabel("Frequency")
@@ -85,16 +110,18 @@ avg_attach_time = np.mean(attach_times)
 
 plt.axvline(avg_attach_time, color='r', linestyle='--', label="Average attach time: {:.2f} ms".format(avg_attach_time))
 
+plt.title(r'$k_{{\mathrm{{react}}}} = {:.3f}$, $\mathrm{{d}}t = {:.2f}$'.format(k_react, dt))
+
 plt.legend()
 
-plt.savefig("attachtime_hist.pdf", format='pdf', bbox_inches='tight')
+plt.savefig("attachtime_hist.{}.pdf".format(k_react), format='pdf', bbox_inches='tight')
 
 plt.clf()
 plt.cla()
 
 plt.figure(figsize=(5, 5))
 
-plt.hist(bounces, bins='auto', color='b', alpha=0.5)
+plt.hist(bounces, bins='auto', color='b', alpha=0.5, density=True)
 
 plt.xlabel("Number of bounces")
 plt.ylabel("Frequency")
@@ -103,6 +130,8 @@ avg_bounces = np.mean(bounces)
 
 plt.axvline(avg_bounces, color='r', linestyle='--', label="Average bounces: {:.2f}".format(avg_bounces))
 
+plt.title(r'$k_{{\mathrm{{react}}}} = {:.3f}$, $\mathrm{{d}}t = {:.2f}$'.format(k_react, dt))
+
 plt.legend()
 
-plt.savefig("bounces_hist.pdf", format='pdf', bbox_inches='tight')
+plt.savefig("bounces_hist.{}.pdf".format(k_react), format='pdf', bbox_inches='tight')
