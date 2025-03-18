1a8e717857e4552a86b38b86bb905b9f7fb2f2b9
Amit Bikram Sanyal <absanyal.ABS@gmail.com>
2025-03-18 15:42:52 -0400

    feat: Enhance multicore submission script to manage job limits and improve simulation tracking

--- submit_multicore_pc.sh
+++ submit_multicore_pc.sh
@@ -1,3 +1,11 @@
+MAX_JOBS=128
+
+wait_for_jobs() {
+    while (( $(jobs -p | wc -l) >= MAX_JOBS )); do
+        sleep 1  # Check every second for a free slot
+    done
+}
+
 make clean
 make
 
@@ -12,7 +20,8 @@ mkdir -p runs
 
 for i in $(seq 1 ${n_sims})
 do
-    echo "Submitting simulation ${i}"
+    wait_for_jobs
+    echo "Submitting simulation ${i} / ${n_sims}"
     mkdir -p runs/run_${i}
     cp input_files_pc/input.${i}.inp runs/run_${i}/input.${i}.inp
     cp filaments runs/run_${i}/filaments
@@ -24,8 +33,12 @@ do
     mkdir -p outrun
     mkdir -p data
     mkdir -p dump
+    
+    (
+    ./filaments input.${i}.inp > out.run.${i}
+    echo "Simulation ${i} complete"
+    ) &
 
-    time ./filaments input.${i}.inp > out.run.${i} &
     cd ../..
 done
 wait
